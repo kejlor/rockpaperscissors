ec17bb44d76a1dff9db399e33e39efe939ea9d14
kejlor <woj450@gmail.com>
2021-12-05 17:47:59 +0100

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Bartosz Wojtkowiak on 05/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Bartosz Wojtkowiak on 05/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
